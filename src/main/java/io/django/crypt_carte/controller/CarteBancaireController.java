d8e0ad2525dccae418c2d44298f70a0793f6fb99
django123 <edougajean@gmail.com>
2025-02-11 19:27:34 +0100

    first commit

--- src/main/java/io/django/crypt_carte/controller/CarteBancaireController.java
+++ src/main/java/io/django/crypt_carte/controller/CarteBancaireController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/carte-bancaire")
+@CrossOrigin(origins = "*")
 public class CarteBancaireController {
 
     private final CarteBancaireService carteBancaireService;
